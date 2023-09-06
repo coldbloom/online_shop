522dd6f89af469086c95b480f6388d23e8b0f461
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2023-09-06 16:54:10 +0300

    secound commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config() // без этой строчки мы не можем обращаться к переменным из .env
+const express = require('express') // импортируем модуль express
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000;
+
+const app = express() // создаем объект express (запуск приложения)
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router);
+
+// обработка ошибок последний Middleware
+app.use(errorHandler)
+
+const start = async () => {
+    try {
+        await sequelize.authenticate() // подключение к базе данных перед запуском сервера
+        await sequelize.sync() // эта ф-я сверяет состояние бд со схемой данных
+        app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
