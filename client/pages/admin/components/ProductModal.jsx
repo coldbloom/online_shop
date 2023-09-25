ce5738e40e0591b31c7231155a4feeb2a63c38b0
unknown <coldbloom22@gmail.com>
2023-09-26 00:19:02 +0300

    developming admin-panel

--- client/pages/admin/components/ProductModal.jsx
+++ client/pages/admin/components/ProductModal.jsx
@@ -5,27 +5,55 @@ const ProductModal = () => {
     const [categories, setCategories] = React.useState([])
     const [selectedItem, setSelectedItem] = React.useState('')
 
+    const [name, setName] = React.useState('')
+    const [price, setPrice] = React.useState('')
+
     React.useEffect(() => {
         axios.get(`${process.env.SERVER_URL}/category`)
             .then(res => setCategories(res.data))
             .catch(error => console.log(error))
     }, [])
 
+    const addNewProduct = () => {
+
+    }
+
     return (
         <div>
             <h3>Создание карточки товара</h3>
-            <select
-                value={selectedItem}
-                onChange={(e) => setSelectedItem(e.target.value)}
+            <div className='flex flex-row'>
+                <p>Выберите категорию для товара:</p>
+                <select
+                    value={selectedItem}
+                    onChange={(e) => setSelectedItem(e.target.value)}
+                >
+                    {categories.map(category =>
+                        <option
+                            key={category.id}
+                            value={category.id}
+                        >
+                            {category.name}
+                        </option>)}
+                </select>
+            </div>
+            <input type="text"
+                   placeholder='Введите наименование товара'
+                   value={name}
+                   onChange={e => setName(e.target.value)}
+                   className='border rounded w-full'
+            />
+            <input type="text"
+                   placeholder='Введите цену товара'
+                   value={price}
+                   onChange={e => setPrice(e.target.value)}
+                   className='border rounded w-full'
+            />
+            <button
+                onClick={() => addNewProduct()}
+                className='bg-blue-400'
             >
-                {categories.map(category =>
-                    <option
-                        key={category.id}
-                        value={category.id}
-                    >
-                        {category.name}
-                    </option>)}
-            </select>
+                Добавить товар
+            </button>
         </div>
     );
 };
--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,28 +1,40 @@
 const { Category } = require('./../models/models')
+const ApiError = require('../error/ApiError')
 
 class CategoryController {
-    async create(req, res) {
-        const {name} = req.body
-        const category = await Category.create({name})
-        return res.json(category)
-    }
-
-    async getAll(req, res) {
-        const categories = await Category.findAll()
-        return res.json(categories)
-    }
-
-    async delete(req, res) {
-        console.log(req.params)
-        const { id } = req.params;
-        const category = await Category.findByPk(id);
-
-        if (!category) {
-            return res.status(401).json({ error: 'Category not found 11111' });
+    async create(req, res, next) {
+        try {
+            const {name} = req.body
+            const category = await Category.create({name})
+            return res.json(category)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
         }
+    }
 
-        await category.destroy();
-        return res.json({ message: 'Category deleted successfully' });
+    async getAll(req, res, next) {
+        try {
+            const categories = await Category.findAll()
+            return res.json(categories)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params;
+            const category = await Category.findByPk(id);
+
+            if (!category) {
+                return res.status(401).json({ error: 'Category not found 11111' });
+            }
+
+            await category.destroy();
+            return res.json({ message: 'Category deleted successfully' });
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
     }
 }
 
