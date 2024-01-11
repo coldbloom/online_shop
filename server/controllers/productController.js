447d70fb26146d00d52ef7d550c3898a4cb49307
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2024-01-11 15:08:32 +0300

    edit is done

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -16,6 +16,10 @@ const getImageName = (path) => {
     return path.substring(path.lastIndexOf('/') + 1)
 }
 
+function sortByOrder(images) {
+    return images.sort((a, b) => a.order - b.order);
+}
+
 const upload = multer({ storage: memoryStorage }); // указывается путь для сохранения файлов в буфферной зоне
 class ProductController {
 
@@ -107,56 +111,81 @@ class ProductController {
                 })
 
                 const requestData = req.body;
-                const updatedImagesNames = JSON.parse(requestData.oldImages).map(item => item.name)
-                const deletedImages = allImagesFromDB.filter(item => !updatedImagesNames.includes(item.name))
-                const newImages = req.files;
 
-                if (deletedImages.length !== 0) {
-                    for (let i = 0; i < deletedImages.length; i++) {
-                        const { name: imageName, id: imageId } = deletedImages[i]
-                        //const imagePath2 = path.join(`media/images/${id}/`, imageName)
-                        const imagePath = `media/images/${id}/${deletedImages[i].name}`
+                let resDataImages = {}
 
-                        //удаляем файл из папки
-                        fs.unlink(imagePath, (error) => {
-                            if (error) {
-                                return next(ApiError.internal(error.message));
-                            }
+                if (Object.keys(requestData).includes('oldImages')) {
+                    const updatedImagesNames = JSON.parse(requestData.oldImages).map(item => item.name)
+                    console.log(requestData.oldImages, ' Все изображения (не файлы)')
+                    // собираем ответ из актуальных изображений из req.data
+                    resDataImages = JSON.parse(requestData.oldImages).map(image => {
+                        return {
+                            id: image.id,
+                            order: image.order,
+                            path: `images/${id}/${image.name}`
+                        }
+                    })
+                    const deletedImages = allImagesFromDB.filter(item => !updatedImagesNames.includes(item.name))
 
-                            //удаляем запись о файле из базы данных
-                            ProductImage.destroy({ where: {id: imageId} })
-                                .then(() => {
-                                    console.log(`Изображение ${imageName} удалено; Запись ProductImage в базе данных с id = ${imageId} удалено!`)
-                                })
-                                .catch((error) => {
-                                    next(ApiError.internal(error.message));
-                                })
-                        });
-                    }
-                }
+                    if (deletedImages.length !== 0) {
+                        for (let i = 0; i < deletedImages.length; i++) {
+                            const { name: imageName, id: imageId } = deletedImages[i]
+                            //const imagePath2 = path.join(`media/images/${id}/`, imageName)
+                            const imagePath = `media/images/${id}/${deletedImages[i].name}`
 
-                // если порядок изображений, запись которых уже есть в базе и файл которых лежит в папке media/images
-                if (requestData.oldImages.length !== 0) {
-                    const updateImages = JSON.parse(requestData.oldImages);
-                    for (let i = 0; i < updateImages.length; i++) {
-                        const { name: imageName, id: imageId, order } = updateImages[i]
+                            //удаляем файл из папки
+                            fs.unlink(imagePath, (error) => {
+                                if (error) {
+                                    return next(ApiError.internal(error.message));
+                                }
 
-                        // Найти запись productImage по идентификатору
-                        const productImage = await ProductImage.findByPk(imageId);
-
-                        if (productImage) {
-                            // Изменить поле order на новое значение
-                            productImage.order = order;
-
-                            await productImage.save();
-                        } else {
-                            console.log(`ProductImage с ${imageId} не найден в Базе Данных`)
-                            return next(ApiError.internal(`ProductImage с ${imageId} не найден в Базе Данных`));
+                                //удаляем запись о файле из базы данных
+                                ProductImage.destroy({ where: {id: imageId} })
+                                    .then(() => {
+                                        console.log(`Изображение ${imageName} удалено; Запись ProductImage в базе данных с id = ${imageId} удалено!`)
+                                    })
+                                    .catch((error) => {
+                                        next(ApiError.internal(error.message));
+                                    })
+                            });
                         }
                     }
+
+                    // если порядок изображений, запись которых уже есть в базе и файл которых лежит в папке media/images
+                    if (requestData.oldImages.length !== 0) {
+                        const updateImages = JSON.parse(requestData.oldImages);
+                        for (let i = 0; i < updateImages.length; i++) {
+                            const { name: imageName, id: imageId, order } = updateImages[i]
+
+                            // Найти запись productImage по идентификатору
+                            const productImage = await ProductImage.findByPk(imageId);
+
+                            if (productImage) {
+                                // Изменить поле order на новое значение
+                                productImage.order = order;
+
+                                await productImage.save();
+                            } else {
+                                console.log(`ProductImage с ${imageId} не найден в Базе Данных`)
+                                return next(ApiError.internal(`ProductImage с ${imageId} не найден в Базе Данных`));
+                            }
+                        }
+                    }
+                } else {
+                    // собираем ответ из актуальных изображений из БД
+                    resDataImages = product.images.map(item => {
+                        return {
+                            path: item.path,
+                            id: item.dataValues.id,
+                            order: item.dataValues.order,
+                        }
+                    })
                 }
 
                 // если пользователь добавил новые изображения в req.files
+                const newImages = req.files;
+                console.log(newImages, ' Все изображения (файлы)')
+
                 if (newImages.length !== 0) {
                     const dir = `media/images/${id}`
 
@@ -164,34 +193,82 @@ class ProductController {
                         return next(ApiError.internal(`Папки с ${id} продукта не найдено media/images`));
                     }
 
-                    await Promise.all(newImages.map(async (file, idx) => {
-                        const imagePath = `${dir}/${file.originalname}`
-                        const order = file.fieldname[file.fieldname.lastIndexOf('=') + 1]
+                    const createdImages = await Promise.all(newImages.map(async (file, idx) => {
+                        const imagePath = `${dir}/${file.originalname}`;
+                        const order = file.fieldname[file.fieldname.lastIndexOf('=') + 1];
 
-                        fs.writeFile(imagePath, file.buffer, { encoding: 'binary' }, async (err) => {
-                            if (err) {
-                                console.error(err);
-                            } else {
-                                await ProductImage.create({
-                                    path: `images/${id}/${file.originalname}`,
-                                    order: order,
-                                    productId: id,
-                                });
-                            }
+                        return new Promise((resolve, reject) => {
+                            fs.writeFile(imagePath, file.buffer, { encoding: 'binary' }, async (err) => {
+                                if (err) {
+                                    console.error(err);
+                                    reject(err);
+                                } else {
+                                    try {
+                                        const createdProductImage = await ProductImage.create({
+                                            path: `images/${id}/${file.originalname}`,
+                                            order: order,
+                                            productId: id,
+                                        });
+                                        resolve(createdProductImage);
+                                    } catch (error) {
+                                        reject(error);
+                                    }
+                                }
+                            });
                         });
-                    }))
+                    }));
+
+                    resDataImages = [...resDataImages, ...createdImages.map(image => ({
+                        id: image.dataValues.id,
+                        order: image.dataValues.order,
+                        path: image.dataValues.path
+                    }))];
                 }
 
+                // если в req.data есть поля которые требуют редактирования, кроме изображений
+                const fields = Object.keys(requestData).filter(key => key !== 'oldImages')
+                if (fields.length !== 0) {
+                    fields.forEach(field => {
+                        switch (field) {
+                            case 'name':
+                                console.log(`name = ${requestData.field}`)
+                                product.name = requestData.name
+                                break;
+                            case 'about':
+                                console.log(`about = ${requestData.field}`)
+                                product.about = requestData.about
+                                break;
+                            case 'price':
+                                console.log(`price = ${requestData.field}`)
+                                product.price = requestData.price
+                                break;
+                            case 'categoryId':
+                                console.log(`categoryId = ${requestData.field}`)
+                                product.categoryId = requestData.categoryId
+                                break;
+                        }
+                    });
 
-                console.log(allImagesFromDB, ' = allImagesFromDB Что лежит в массиве id и name всех фотогравий из бд')
+                    // Сохранение изменений в базе данных с использованием Sequelize
+                    try {
+                        await product.save(); // Используем метод save для сохранения обновленных данных в базе
+                        console.log('Изменения сохранены в базе данных');
+                    } catch (error) {
+                        console.error('Ошибка при сохранении изменений в базе данных:', error);
+                    }
+                }
 
-                console.log(' patch update product request')
-                console.log(requestData, ' = requestData = req.body')
-                console.log(newImages, ' = req.files')
+                console.log(product.images, ' изображения после редактирования - АКТУАЛЬНОЕ !!!')
 
-
-
-                return res.json('update product')
+                const resData = {
+                    id: Number(id),
+                    name: product.name,
+                    price: product.price,
+                    categoryId: product.categoryId,
+                    about: product.about,
+                    images: sortByOrder(resDataImages)
+                }
+                return res.json(resData)
             })
         } catch (e) {
             next(ApiError.badRequest(e.message))
